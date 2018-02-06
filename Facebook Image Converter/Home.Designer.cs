fede143925477094cf031032c0b8c1b8456b4cf2
Nguyen Hau <nguyenhau0596@gmail.com>
2018-02-06 23:51:48 +0700

    Init project

---
+++ Facebook Image Converter/Home.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Facebook_Image_Converter
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.defaultConfigsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ouptToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelTotalImages = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.configsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(739, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFilesToolStripMenuItem,
+            this.openFolderToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFilesToolStripMenuItem
+            // 
+            this.openFilesToolStripMenuItem.Name = "openFilesToolStripMenuItem";
+            this.openFilesToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.openFilesToolStripMenuItem.Text = "Open files";
+            this.openFilesToolStripMenuItem.Click += new System.EventHandler(this.openFilesClick);
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.openFolderToolStripMenuItem.Text = "Open folder";
+            this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.openFolderClick);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.closeApp);
+            // 
+            // configsToolStripMenuItem
+            // 
+            this.configsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.defaultConfigsToolStripMenuItem,
+            this.ouptToolStripMenuItem});
+            this.configsToolStripMenuItem.Name = "configsToolStripMenuItem";
+            this.configsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.configsToolStripMenuItem.Text = "Settings";
+            // 
+            // defaultConfigsToolStripMenuItem
+            // 
+            this.defaultConfigsToolStripMenuItem.Name = "defaultConfigsToolStripMenuItem";
+            this.defaultConfigsToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.defaultConfigsToolStripMenuItem.Text = "Default configs";
+            // 
+            // ouptToolStripMenuItem
+            // 
+            this.ouptToolStripMenuItem.Name = "ouptToolStripMenuItem";
+            this.ouptToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.ouptToolStripMenuItem.Text = "Themes";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // labelTotalImages
+            // 
+            this.labelTotalImages.AutoSize = true;
+            this.labelTotalImages.Location = new System.Drawing.Point(12, 35);
+            this.labelTotalImages.Name = "labelTotalImages";
+            this.labelTotalImages.Size = new System.Drawing.Size(101, 20);
+            this.labelTotalImages.TabIndex = 1;
+            this.labelTotalImages.Text = "0 item selected";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Convert);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 415);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelTotalImages);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Home";
+            this.Text = "Facebook Image Converter";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem defaultConfigsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ouptToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label labelTotalImages;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Facebook Image Converter/Home.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Facebook_Image_Converter
+{
+    public partial class Home : Form
+    {
+        String[] selected_files;
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void openFilesClick(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            //openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.Multiselect = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            // Insert code to read the stream here.
+                            selected_files = openFileDialog1.FileNames;
+                            labelTotalImages.Text = selected_files.Length.ToString() 
+                                + " item"+(selected_files.Length>1?"s":"")+" selected.";
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void openFolderClick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog openFileDialog1 = new FolderBrowserDialog();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (openFileDialog1)
+                    {
+                        // Insert code to read the stream here.
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read folder from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void closeApp(object sender, EventArgs e)
+        {
+            //do something before closing app
+
+            Application.Exit();
+        }
+
+        private void Convert(object sender, EventArgs e)
+        {
+            for (var i = 0; i < selected_files.Length; i++)
+            {
+                var png = Image.FromFile(selected_files[i]);
+                
+                png.Save("ouput"+i.ToString()+".png", ImageFormat.Png);
+            }
+        }
+    }
+}
